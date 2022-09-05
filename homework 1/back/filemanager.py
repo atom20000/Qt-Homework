c077a3b52693d5bef490f413d8eb987143b44e9b
atom20000 <0362725@mail.ru>
2022-09-05 20:39:51 +0300

    Write base method working with files and decorator for check exist file

---
+++ homework 1/back/filemanager.py
@@ -0,0 +1,29 @@
+import csv
+import os
+
+
+class WorkFile():
+
+
+    def __init__(self, path):
+        self.path = path
+    
+    def check_exist_file(method):
+        
+        def check_exec(self):
+            if os.path.exists(self.path) and os.path.isfile(self.path):
+                return method(self)
+            else:
+                return None
+        return check_exec
+
+    @check_exist_file
+    def read(self):
+        with open(self.path, 'r') as file:
+            return file.read()
+    
+    @check_exist_file
+    def write(self, data):
+        with open(self.path, 'wa') as file:
+            file.write(data) 
+    
