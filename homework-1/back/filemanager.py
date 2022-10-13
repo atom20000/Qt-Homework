debd256d810a32b5cde5241e8059618343e03c35
atom20000 <0362725@mail.ru>
2022-10-13 22:23:45 +0300

    Implement write function

--- homework-1/back/filemanager.py
+++ homework-1/back/filemanager.py
@@ -1,17 +1,17 @@
 import csv
 import os
 
-class WorkFile():
 
+class WorkFile():
 
     def __init__(self, path):
         self.path = path
     
     def check_exist_file(method):
         
-        def check_exec(self):
+        def check_exec(self, *args, **kwargs):
             if os.path.exists(self.path) and os.path.isfile(self.path):
-                return method(self)
+                return method(self, *args, **kwargs)
             else:
                 return None
         return check_exec
@@ -20,17 +20,17 @@ class WorkFile():
     def read(self):
         with open(self.path, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
-            outdict = {i:[] for i in reader.fieldnames}
+            outdict = {i : [] for i in reader.fieldnames}
             for row in reader:
                 for item in row.items():
                      outdict[item[0]].append(item[1])
             return outdict
     
-    @check_exist_file
-    def write(self, data):
+    #@check_exist_file
+    def write(self, data: dict, count_rows):
         with open(self.path, 'w') as csvfile:
-            csv.writer(csvfile).writerows(data)
-            #TODO
-
-if __name__ == '__main__':
-    print(WorkFile('/home/atom/atom/Qt-Homework/homework-1/back/addresses.csv').read())
+            filenames = data.keys()
+            writer = csv.DictWriter(csvfile, filenames)
+            writer.writeheader()
+            for r in range(count_rows):
+                writer.writerow({i:data[i][r] for i in filenames})
