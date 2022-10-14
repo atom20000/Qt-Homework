df284dff69cb726862c54904a039c0f5062fe28b
atom20000 <0362725@mail.ru>
2022-10-14 23:47:55 +0300

    Emplement addcolumns function

--- homework-1/back/model.py
+++ homework-1/back/model.py
@@ -7,11 +7,15 @@ class CSVModel(QAbstractTableModel):
 
     @property
     def match_keys(self):
-        return { i:j for i, j in zip(range(self.columnCount()), self._QMap.keys())}
+        return { i:j for i, j in zip(range(self.max_colums), self._QMap.keys())}
 
     @property
     def max_row(self):
         return len(max(self._QMap.values(), key= lambda item: len(item), default=[]))
+
+    @property
+    def max_colums(self):
+        return len(self._QMap)
 
     def __init__(self, parent: typing.Optional[QObject] = QObject(), QMap = {}) -> None:
         super().__init__(parent)
@@ -22,15 +26,17 @@ class CSVModel(QAbstractTableModel):
         return self.max_row if not parent.isValid() else 0
 
     def columnCount(self, parent: QModelIndex = QModelIndex()) -> int:
-        return len(self._QMap) if not parent.isValid() else 0
+        return self.max_colums if not parent.isValid() else 0
 
     def headerData(self, section: int, orientation: Qt.Orientation, role: int = Qt.ItemDataRole.DisplayRole) -> typing.Any:
         if role != Qt.ItemDataRole.DisplayRole:
             return  QVariant()
-        if orientation == Qt.Orientation.Horizontal:
+        if orientation == Qt.Orientation.Horizontal and role == Qt.ItemDataRole.DisplayRole:
             return QVariant(list(self._QMap.keys())[section])
-        elif orientation == Qt.Orientation.Vertical:
+        elif orientation == Qt.Orientation.Vertical and role == Qt.ItemDataRole.DisplayRole:
             return QVariant('  ')
+        if orientation == Qt.Orientation.Horizontal and role == Qt.ItemDataRole.EditRole:
+            return QVariant(list(self._QMap.keys())[section])
         return QVariant()
 
     def data(self, index: QModelIndex, role: int = Qt.ItemDataRole.DisplayRole) -> typing.Any:
@@ -44,16 +50,20 @@ class CSVModel(QAbstractTableModel):
 
     def insertRows(self, row: int, count: int, parent: QModelIndex = QModelIndex()) -> bool:
         super().beginInsertRows(QModelIndex(), row, row + count - 1)
+        if len(self._QMap.keys()) == 0:
+            self.insertColumns(0, 1)
         for r in range(row, row + count):
             for i in self._QMap.keys():
                 self._QMap[i].insert(row, '')
-            super().endInsertRows()
+        super().endInsertRows()
         return True
 
     def insertColumns(self, column: int, count: int, parent: QModelIndex = QModelIndex()) -> bool:
-        return False
         super().beginInsertColumns(QModelIndex(), column, column + count - 1)
-        #TODO
+        tmp_lst = list(self._QMap.items())
+        for c in range(column, column + count):
+            tmp_lst.insert(column, ( self.generate_header_name(), ['' for i in range(self.max_row)]))
+        self._QMap = {i[0] : i[1] for i in tmp_lst}
         super().endInsertColumns()
         return True
 
@@ -77,3 +87,17 @@ class CSVModel(QAbstractTableModel):
             self._QMap[self.match_keys[index.column()]][index.row()] = str(value)
             super().dataChanged.emit(index, index, [Qt.ItemDataRole.DisplayRole, Qt.ItemDataRole.EditRole])
             return True
+    
+    def setHeaderData(self, section: int, orientation: Qt.Orientation, value: typing.Any, role: int = Qt.ItemDataRole.EditRole) -> bool:
+        if role == Qt.ItemDataRole.EditRole and orientation == Qt.Orientation.Horizontal:
+            data = self._QMap.pop([self.match_keys[section]])
+            tmp_lst = list(self._QMap.items())
+            tmp_lst.insert(section, ( value, data))
+            self._QMap = {i[0] : i[1] for i in tmp_lst}
+            super().headerDataChanged.emit(section, section, [Qt.ItemDataRole.DisplayRole, Qt.ItemDataRole.EditRole])
+            return True
+    
+    def generate_header_name(self):
+        count = 1
+        while ' '*count in self._QMap.keys(): count+=1
+        return ' '*count
