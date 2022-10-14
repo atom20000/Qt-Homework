0053464454bcf744ffbb196de82d56c8a29585cd
atom20000 <0362725@mail.ru>
2022-10-14 23:48:40 +0300

    Emplement save and addcolumns function

--- homework-1/gui/MainWindow.py
+++ homework-1/gui/MainWindow.py
@@ -7,13 +7,14 @@ class Main(QMainWindow):
     
     def __init__(self, ui: Ui_CsvEditor) -> None:
         super().__init__()
+        self.path = ''
         self.ui = ui
         self.ui.setupUi(self)
         self.model = CSVModel()
         self.ui.tableView.setModel(self.model)
         self.ui.actionQuit.triggered.connect(QApplication.instance().quit)
         self.ui.actionOpen.triggered.connect(self.open_file)
-        #self.ui.actionSave.triggered.connect()
+        self.ui.actionSave.triggered.connect(self.save)
         self.ui.actionSave_as.triggered.connect(self.save_as_file)
         self.ui.actionAddRows.triggered.connect(self.add_rows)
         self.ui.actionAddColumns.triggered.connect(self.add_columns)
@@ -23,11 +24,21 @@ class Main(QMainWindow):
         #self.ui.tableView.selectionModel().selectionChanged.connect(lambda x, y: print(self.ui.tableView.selectionModel().selectedRows()))
 
     def open_file(self):
-        self.model = CSVModel(QMap= WorkFile(QFileDialog.getOpenFileName(filter='*.csv *.CSV')[0]).read())
-        self.ui.tableView.setModel(self.model)
+        self.path = QFileDialog.getOpenFileName(filter='*.csv *.CSV')[0]
+        if self.path != '':
+            self.model = CSVModel(QMap= WorkFile(self.path).read())
+            self.ui.tableView.setModel(self.model)
+
+    def save(self):
+        if self.path != '':
+            WorkFile(self.path).write(self.model._QMap, self.model.max_row)
+        else:
+            self.save_as_file()
 
     def save_as_file(self):
-        WorkFile(QFileDialog.getSaveFileName(filter='*.csv *.CSV')[0]).write(self.model._QMap, self.model.max_row)
+        self.path = QFileDialog.getSaveFileName(filter='*.csv *.CSV')[0]
+        if self.path != '':
+            WorkFile(self.path).write(self.model._QMap, self.model.max_row)
 
     def add_rows(self):
         if (select_rows := self.ui.tableView.selectionModel().selectedRows()) and len(select_rows) !=0:
@@ -37,8 +48,11 @@ class Main(QMainWindow):
             self.model.insertRows(self.model.max_row, 1)
 
     def add_columns(self):
-        #TODO
-        pass
+        if (select_columns := self.ui.tableView.selectionModel().selectedColumns()) and len(select_columns) !=0:
+            for i in select_columns:
+                self.model.insertColumns(i.column() + 1, 1)
+        else:
+            self.model.insertColumns(self.model.max_colums, 1)
 
     def remove_rows(self):
         if (select_rows := self.ui.tableView.selectionModel().selectedRows()) and len(select_rows) !=0:
